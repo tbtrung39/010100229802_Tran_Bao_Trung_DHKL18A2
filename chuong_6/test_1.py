7e7d43ba29a299f2182208805172684c4ee36e32
O.T <31383935+Oguri39@users.noreply.github.com>
2024-10-14 12:13:03 +0700

    Bo sung luu y

--- chuong_6/test_1.py
+++ chuong_6/test_1.py
@@ -1,18 +1,28 @@
-# a = 1
-# b = 1
+#Khai báo biến
+a = 1
+b = 1
+#Khi đặt tên phải tuân thủ các quy tắc sau
+# - Sử dụng chữ viết thường không dấu
+# - Sử dụng dấu _ thay cho khoảng cách giữa 2 chữ
+# - Sử dụng chữ viết hoa chỉ khi muốn nhấn mạnh giá trị biến không được phép thay đổi (VD: POST_TIME = 1)
+# - Không được đặt tên trùng với các biến đã được khai báo hoặc các câu lệnh có sẵn trong Python
+# - Không được đặt tên bắt đầu bằng các số hoặc đặt tên chứa các kí tự đặc biệt
+# - Đặt tên phải có ý nghĩa
 
+#Đặt tên biến đúng:
+di_hoc = 1
+DI_HOC = 2
 
-# di_hoc = 1
+#Đặt tên biến sai
+# biến = 1
+# if = 1
+# else = 2
+# print = 1
+# 123Abv = 2
+# a%2&2]as = 1
 
-# DI_HOC = 2
-# # if = 1
-# # else = 2
-
-# # giai phuong trinh bac nhat ax+b = c
-# # he_so_a = 1
-# # he_so_b = 2
-# # he_so_c = 3
-
+#Kiểu dữ liệu:
+#Trong python kiểu dữ liệu của một biến chỉ được xác định khi biến đó được gán giá trị
 bien_int = 3 #dinh dang int (integer) - kieu so nguyen: ...-2, -1, 0, 1, 2,...
 bien_float = 4.678 #dinh dang float - kieu so thuc: ....-2.4, -2.324,...,0, 1.0, 1.1
 bien_string = "Day la kieu ky tu" #dinh dang str (string) - kieu ky tu: abcdef....
@@ -28,5 +38,5 @@ kieu_tuple  = (1,2,"abc") # dinh dang tuple
 kieu_set = {1,2,"abc"} # dinh dang set (tap hop)
 
 
-
+#Kiểm tra kiểu dữ liệu của một biến bất kì
 print(type(bien_float))
