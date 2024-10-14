7057420a2e2adaefece407a5180886de9a36670b
O.T <31383935+Oguri39@users.noreply.github.com>
2024-10-14 11:55:26 +0700

    lam hoan thanh bai 1 - chuong 6

---
+++ chuong_6/test_1.py
@@ -0,0 +1,32 @@
+# a = 1
+# b = 1
+
+
+# di_hoc = 1
+
+# DI_HOC = 2
+# # if = 1
+# # else = 2
+
+# # giai phuong trinh bac nhat ax+b = c
+# # he_so_a = 1
+# # he_so_b = 2
+# # he_so_c = 3
+
+bien_int = 3 #dinh dang int (integer) - kieu so nguyen: ...-2, -1, 0, 1, 2,...
+bien_float = 4.678 #dinh dang float - kieu so thuc: ....-2.4, -2.324,...,0, 1.0, 1.1
+bien_string = "Day la kieu ky tu" #dinh dang str (string) - kieu ky tu: abcdef....
+
+kieu_boolean_1 = True   #dinh dang bool (Boolean) - kieu lua chon True, False 
+kieu_boolean_2 = False
+
+kieu_none = None  #dinh dang khai bao khong dinh dang
+
+kieu_list = [1,2,"abc"] #dinh dang list (danh sach) - kieu du lieu tuan tu dang danh sach: [1,"abc", 4.6]
+kieu_dictionary = {"khoa": "gia tri", "a": 1} #dinh dang kieu dict (tu dien)
+kieu_tuple  = (1,2,"abc") # dinh dang tuple
+kieu_set = {1,2,"abc"} # dinh dang set (tap hop)
+
+
+
+print(type(bien_float))
