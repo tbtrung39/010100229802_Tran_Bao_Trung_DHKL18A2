9586bb27e129cb53a0a6d79160dde239435dc193
O.T <31383935+Oguri39@users.noreply.github.com>
2024-10-14 12:15:33 +0700

    Bo sung luu y

--- chuong_6/test_1.py
+++ chuong_6/test_1.py
@@ -23,19 +23,19 @@ DI_HOC = 2
 
 #Kiểu dữ liệu:
 #Trong python kiểu dữ liệu của một biến chỉ được xác định khi biến đó được gán giá trị
-bien_int = 3 #dinh dang int (integer) - kieu so nguyen: ...-2, -1, 0, 1, 2,...
-bien_float = 4.678 #dinh dang float - kieu so thuc: ....-2.4, -2.324,...,0, 1.0, 1.1
-bien_string = "Day la kieu ky tu" #dinh dang str (string) - kieu ky tu: abcdef....
+bien_int = 3 #Định dạng int (integer) - kiểu số nguyên: ...-2, -1, 0, 1, 2,...
+bien_float = 4.678 #Định dạng float - kiểu số thực: ....-2.4, -2.324,...,0, 1.0, 1.1
+bien_string = "Day la kieu ky tu" #Định dạng str (string) - kiểu ký tự: abcdef....
 
-kieu_boolean_1 = True   #dinh dang bool (Boolean) - kieu lua chon True, False 
+kieu_boolean_1 = True   #Định dạng bool (Boolean) - kiểu lựa chọn True, False 
 kieu_boolean_2 = False
 
-kieu_none = None  #dinh dang khai bao khong dinh dang
+kieu_none = None  #Định dạng khai báo không định dạng
 
-kieu_list = [1,2,"abc"] #dinh dang list (danh sach) - kieu du lieu tuan tu dang danh sach: [1,"abc", 4.6]
-kieu_dictionary = {"khoa": "gia tri", "a": 1} #dinh dang kieu dict (tu dien)
-kieu_tuple  = (1,2,"abc") # dinh dang tuple
-kieu_set = {1,2,"abc"} # dinh dang set (tap hop)
+kieu_list = [1,2,"abc"] #Định dạng list (danh sách) - kiểu dữ liệu tuần tự danh sách: [1,"abc", 4.6]
+kieu_dictionary = {"khoa": "gia tri", "a": 1} #Định dạng kiểu dict (từ điển) - kiểu dữ liệu tuần tự từ điển
+kieu_tuple  = (1,2,"abc") # Định dạng tuple - kiểu dữ liệu tuần tự tuple
+kieu_set = {1,2,"abc"} # Định dạng set (tập hợp) - kiểu dữ liệu tuần tự tập hợp
 
 
 #Kiểm tra kiểu dữ liệu của một biến bất kì
