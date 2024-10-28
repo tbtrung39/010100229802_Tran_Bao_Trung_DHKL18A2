4e29aee65d32f30ab82c7025195b902c2630410e
O.T <31383935+Oguri39@users.noreply.github.com>
2024-10-28 09:48:52 +0700

    add bai_baitap

---
+++ 08_bai_tap_cau_lenh_dieu_kien_mau.py
@@ -0,0 +1,31 @@
+# Câu 1
+# Nhập vào 1 số nguyên. Kiểm tra xem nó là số chẵn hay số lẻ.
+
+# Câu 2
+# Nhập vào 2 số nguyên. Kiểm tra xem chúng cùng dấu hay trái dấu.
+
+# Câu 3
+# Giải phương trình bậc nhất ax+b=0 (với a, b là kiểu số nguyên).
+
+# Câu 4
+# Giải phương trình bậc hai a*x**2+b*x+c=0 (với a, b, c là kiểu số nguyên).
+
+# Câu 5
+# Cho tọa độ 3 điểm A(x1, y1, z1), B(x2, y2, z2), C(x3, y3, z3). 
+# Với các giá trị x1, x2, x3, y1, y2, y3, z1, z2, z3, tìm khoảng cách giữa các điểm này
+
+# Câu 6
+# Cho tọa độ I(x,y), bán kính R và một điểm M(a,b). 
+# Với các giá trị a, b, x, y, R bất kì, tìm vị trí của M so với mặt cầu tâm I bán kính R
+
+# Câu 7
+# Cho tọa độ 2 điểm A(x1, y1), B(x2, y2). 
+# Với các giá trị x1,x2,y1,y2 bất kỳ, tìm phương trình tổng quát của đường thẳng đi qua 2 điểm này.
+
+# Câu 8
+# Viết chương trình tính điểm của sinh viên.
+# Chương trình này sẽ đọc vào các loại điểm của sinh viên (điểm chuyên cần, điểm giữa kỳ, và điểm cuối kỳ) và xếp loại điểm theo quy luật sau:
+# – if điểm trung bình >=9 =>loại=A
+# – if điểm trung bình >= 7 và < 9 => loại=B
+# – if điểm trung bình >= 5 and < 7 =>loại=C
+# – if điểm trung bình < 5 => loại=D
---
+++ 08_bai_tap_ve_nha_cau_lenh_dieu_kien.py
@@ -0,0 +1,54 @@
+# 1. Tính năm nhuận. Năm thứ n là nhuận nếu nó chia hết cho 4, nhưng không chia hết cho 100 hoặc chia hết 400. 
+
+# 2. Viết chương trình kiểm tra xem điểm M(x,y) có nằm trong hình tròn tâm I(a,b) và bán kính 
+# R bằng cách xuất ra giá trị True nếu điểm M nằm trong hoặc trên hình tròn và False nếu nằm ngoài hình tròn, 
+# với x, y, a, b, R nhập vào từ bàn phím?
+
+# 3. Viết chương trình tìm số lớn nhất trong 3 số bằng Python
+
+# 4. Viết chương trình nhập vào các số a, b, c, sau đó kiểm tra bộ ba số a, b, c vừa nhập vào 
+# là bộ ba cạnh của tam giác thường, tam giác vuông, tam giác cân, tam giác vuông cân, tam giác đều 
+# hay không phải là bộ ba cạnh của tam giác.
+
+# 5. Viết chương trình kiểm tra một ký tự trong bảng chữ cái tiếng anh là nguyên âm hay phụ âm. 
+# Ký tự là bất kỳ được nhập từ bàn phím.
+
+# 6. Viết chương trình thể hiện menu lựa chọn gồm các thể loại phim hiện đang có trong rạp chiếu phim ABC. 
+# Yêu cầu người dùng nhập lựa chọn thể loại phim muốn xem (Phim tình cảm, Phim kinh dị, Phim hoạt hình, Phim khoa học viễn tưởng)
+
+
+# 7. Viết chương trình giải hệ phương trình 2 ẩn:
+# 	    a_1*x + b_1*y = c_1
+#       a_2*x + b_2*y = c_2
+# Các hệ số a1, a2, b1, b2, c1, c2 nhập từ bàn phím. Xét tất cả các trường hợp cụ thể
+# Công thức Cramer2 dùng tính hệ phương trình 2 ẩn
+
+# 8. Viết chương trình phân loại sinh viên dựa vào kết quả điểm học tập. 
+# Nếu điểm A thì phân loại là sinh viên xuất sắc, 
+# điểm B là sinh viên loại giỏi, 
+# điểm C là sinh viên loại khá, 
+# điểm D là sinh viên loại trung bình, 
+# điểm E là sinh viên loại yếu, 
+# điểm F là sinh viên xếp loại kém.
+
+# 9. Tính cước tacxi: 
+# Viết chương trình tính cước taxi theo biểu phí cơ bản như sau:
+# -	Loại xe 4 chỗ
+#   Giá mở cửa			11.000 đồng/0.8km
+#   Trong phạm vi 20km 	12.100đ/km
+#   Từ km thứ 21 trở đi 		10.000 đồng/km
+# -	Loại  xe 7 chỗ
+#   Giá mở cửa			13.000 đồng/0.8km
+#   Trong phạm vi 30km 	14.100đ/km
+#   Từ km thứ 31 trở đi 		12.000 đồng/km
+# Tiền chờ: 05 phút đầu được miễn phí, từ phút thứ sáu trở đi là 800đ/phút.
+# Loại xe chỉ nhập 4 hoặc 7.
+
+
+# 11. Viết chương trình nhập lương nhân viên, tính thuế thu nhập và lương ròng (số tiền
+# lương thực sự mà nhân viên đó nhận được).
+# Với các thông số giả sử như sau
+# • 30% thuế thu nhập nếu lương là 15 triệu.
+# • 20% thuế thu nhập nếu lương từ 7 đến 15 triệu.
+# • 10% thuế thu nhập nếu lương dưới 7 triệu.
+
