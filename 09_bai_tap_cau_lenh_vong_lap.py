49b685d58c2007c9f172c53c7a2062cbd0a37c58
O.T <31383935+Oguri39@users.noreply.github.com>
2024-11-01 10:49:58 +0700

    add for

---
+++ 09_bai_tap_cau_lenh_vong_lap.py
@@ -0,0 +1,148 @@
+#In dãy số các số lẻ nhỏ hơn n
+n = int(input("Nhap vao so nguyen duong n: "))
+for i in range(n): #-> chuỗi chạy từ 0 đến n-1
+    if i%2 == 1:
+        print(i)    
+#In n các số Fibonacci 
+a = 0
+b = 1
+n = int(input("Nhap vao so nguyen duong n: "))
+for i in range(n):
+    print(a)
+    sum_a_b = a + b
+    a = b
+    b = sum_a_b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Tính tổng các số hạng từ 1 đến n
+#Tính giai thừa của số n
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Nhập vào 2 số a,b Tìm ước chung lớn nhất của hai số này
+# Kiểm tra số n có phải số nguyên tố hay không
---
+++ 09_cau_lenh_vong_lap_for.py
@@ -0,0 +1,15 @@
+#Vòng lặp trong Python
+#Vòng lặp có giới hạn (vòng lặp for)
+
+#các kiểu dữ liệu tuần tự: string, list, tuple, set, dictionary
+# range()
+for abc in range(10):
+    print(abc)
+#range(10) -> 0,1,2,3,4,5,6,7,8,9
+#range khi truyền giá trị mặc định yêu cầu phải là giá trị nguyên dương
+#các giá trị trong range sẽ chạy từ 0 đến n - 1
+
+
+
+
+#Vòng lặp không giới hạn (vòng lặp while)
