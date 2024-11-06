eb8d525d9a34b40d084cb8aa15f0481301099ebf
O.T <31383935+Oguri39@users.noreply.github.com>
2024-11-06 11:01:53 +0700

    edit 09

--- 09_bai_tap_cau_lenh_vong_lap.py
+++ 09_bai_tap_cau_lenh_vong_lap.py
@@ -13,136 +13,52 @@ for i in range(n):
     a = b
     b = sum_a_b
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #Tính tổng các số hạng từ 1 đến n
-#Tính giai thừa của số n
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Nhập vào 2 số a,b Tìm ước chung lớn nhất của hai số này
+tong_s = 0
+n = int(input("Nhap vao gia tri nguyen duong n: "))
+for i in range(n + 1):
+    tong_s = tong_s + i 
+
+print(f"Tong cac so tu 1 den n {tong_s}")
+#Tính giai thừa của số n (n!)
+tich_p = 1
+n = int(input("Nhap vao gia tri nguyen duong n: "))
+for i in range(n + 1):
+    if i == 0:
+        continue
+    tich_p = tich_p*i 
+
+print(f"Tich giai thừa của số n {tich_p}")
+
+
+
+
+# # Nhập vào 2 số a,b Tìm ước chung lớn nhất của hai số này
+a = int(input("Nhap vao so nguyen duong a: " ))
+b = int(input("Nhap vao so nguyen duong b: " ))
+so_nho_nhat = a
+if b <= a:
+    so_nho_nhat = b
+k = so_nho_nhat
+for i in range(so_nho_nhat):
+    if a % k == 0 and b % k == 0:
+        print(f"{k} la uoc chung lon nhat")
+        break
+    k = k - 1
+    
 # Kiểm tra số n có phải số nguyên tố hay không
+# số nguyên tố là số nguyên dương lớn hơn 1 và chỉ chia hết cho 1 và chính nó
+
+n = int(input("Nhap vao so nguyen duong can kiem tra: "))
+if n <= 1:
+    print("So nay khong phai la so nguyen to")
+else:
+    k = n
+    for i in range(n):
+        if n % k == 0 and k != n and k != 1:
+            print("So nay khong phai la so nguyen to")
+            break
+        k = k - 1
+    else:
+        print("So nay la so nguyen to")
+            
--- 09_cau_lenh_vong_lap_for.py
+++ 09_cau_lenh_vong_lap_for.py
@@ -3,13 +3,41 @@
 
 #các kiểu dữ liệu tuần tự: string, list, tuple, set, dictionary
 # range()
-for abc in range(10):
-    print(abc)
+# for abc in range(10):
+#     print(abc)
 #range(10) -> 0,1,2,3,4,5,6,7,8,9
 #range khi truyền giá trị mặc định yêu cầu phải là giá trị nguyên dương
 #các giá trị trong range sẽ chạy từ 0 đến n - 1
 
+#Khi sử dụng vong lặp nên kết hợp sử dụng với các câu lệnh điều kiện
+#(Khi sử dụng vòng lặp nên có mục đích rõ ràng)
 
+#Trong python vòng lặp cung cấp cho người dùng 3 công cụ: break, continue, else
+#break: Dừng vòng lặp ngay lập tức thoát tại lần lặp gặp break
+for i in range(10):
+    if i == 4:
+        break
+    print(i)
 
+#continue: Vòng lặp sẽ bỏ qua lần lặp mà ở đấy xuất hiện continue
+for i in range(10):
+    if i == 4:
+        continue
+    print(i)
 
+#else: Vòng lặp sẽ chạy các câu lệnh xử lý của else trong trường hợp 
+#      vòng lặp không gặp break
+for i in range(10):
+    if i == 4:
+        break
+    print(i)
+else:
+    print("Chay cau lenh else")
+    
+for i in range(10):
+    if i == 4:
+        continue
+    print(i)
+else:
+    print("Chay cau lenh else")
 #Vòng lặp không giới hạn (vòng lặp while)
